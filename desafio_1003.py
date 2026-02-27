d780234b9b41b8ea36445fad04513c52ff44b752
Gelinh <gelinho224@gmail.com>
2026-02-27 10:44:48 -0300

    Update desafio_1003.py

--- desafio_1003.py
+++ desafio_1003.py
@@ -15,3 +15,10 @@ de A e B.
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1003
 
 # Escreva sua solução abaixo
+
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite um numero: "))
+
+SOMA = n1 + n2
+
+print(SOMA)
